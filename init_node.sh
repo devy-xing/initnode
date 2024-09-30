434a306fbd3198b818c483efa12e8513f1de79ad
devy xing <devy.xing@gmail.com>
2024-09-30 17:35:27 +0800

    do some change

--- init_node.sh
+++ init_node.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-# Update package list
-apt-get update
+hemi_ver='heminetwork_v0.4.3_linux_amd64'
+curl -LO https://github.com/hemilabs/heminetwork/releases/download/v0.4.3/${hemi_ver}.tar.gz
+tar -xvzf ${hemi_ver}.tar.gz
+mv ${hemi_ver} hemi
+cd hemi
 
-# Install necessary dependencies (example for Ethereum node)
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:ethereum/ethereum
-apt-get update
-apt-get install -y ethereum
 
-# Start the Ethereum node (example command)
-geth --syncmode "fast" --http --http.api "eth,web3" &
+default_file="popm-address0.json"
+./keygen -secp256k1 -json -net="testnet" > ~/${default_file}
+
+priv_key=$(jq -r '.private_key' ${default_file})
+address=$(jq -r '.pubkey_hash' ${default_file})
+
+echo $priv_key > /tmp/hemi_info.txt
+echo $address >> /tmp/hemi_info.txt
+
+sleep 600
+
+export POPM_BTC_PRIVKEY=${priv_key}
+export POPM_STATIC_FEE=50
+export POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public
+./popmd
